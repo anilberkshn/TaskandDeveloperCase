58b2c1b1b91fc36c9277fb36ece6c517bcd83307
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-13 15:35:30 +0300

    DeveloperService.cs

--- Services/DeveloperService.cs
+++ Services/DeveloperService.cs
@@ -5,35 +5,50 @@ namespace Case2GK20221102.Services;
 
 public class DeveloperService
 {
-     private readonly List<Developer> _developersList = new List<Developer>();
      private GenericRepository<Developer> _repositoryDeveloper;
 
-     public DeveloperService()
+     public DeveloperService(GenericRepository<Developer> genericRepository)
      {
-          _repositoryDeveloper = new GenericRepository<Developer>(_developersList);
+          _repositoryDeveloper = genericRepository;
      } 
-     public Guid Add(Developer developer)
+     
+     public Guid Add(string[] developerParts)
      {
-          return Guid.Empty; // gecici
+          var developer = new Developer
+          {
+               Name = developerParts[1],
+               Surname = developerParts[2],
+               Department =Convert.ToInt32(developerParts[3])
+          };
+           
+          _repositoryDeveloper.Add(developer);
+          return developer.Id;
      }
 
      public Developer? Get(string id)
      {
-          return null;
+          return _repositoryDeveloper.GetById(Guid.Parse(id)); 
      }
      
-     public bool Upsert(Developer developer)
+     public bool Update (string[] developerParts)
      {
-          return true;          // gecici
+          var developer = new Developer
+          {
+               Id = Guid.Parse(developerParts[1]),
+               Name = developerParts[2],
+               Surname = developerParts[3],
+               Department =Convert.ToInt32(developerParts[4])
+          };
+          return _repositoryDeveloper.Update(developer);
      }
 
-     public bool Delete(Developer developer)
+     public bool Delete(string[] developerParts)
      {
-          return false;          // gecici
+          return _repositoryDeveloper.Delete(Guid.Parse(developerParts[1]));          
      }
 
-     public Developer[] GetAll()
+     public List<Developer> GetAll()
      {
-          return _developersList.ToArray();            //geçici
+          return _repositoryDeveloper.GetAll();
      }
 }
