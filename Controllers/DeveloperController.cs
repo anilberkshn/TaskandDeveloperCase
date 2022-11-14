fb69af32037352d69283ed01e741c8aa8c121dbc
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-14 18:14:02 +0300

    DeveloperController.cs yazma denemeleri v1

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -1,4 +1,6 @@
+using Case2GK20221102.Entities;
 using Case2GK20221102.Services;
+using Task = Case2GK20221102.Entities.Task;
 
 namespace Case2GK20221102.Controllers;
 
@@ -11,5 +13,23 @@ public class DeveloperController
     {
         _developerService = developerService;
         _taskService = taskService;
+    }
+
+    public void ControlDeveloper(string[] developerParts)
+    {
+        if (developerParts[1] == "dev" && developerParts[0] == "delete")
+        {
+            var task = new Task();
+            _developerService.Delete(developerParts);
+           // if (developerParts[2] == _taskService.Get(developerParts[2]))
+            {
+                
+            }
+            
+        }
+
+       
+
+        
     }
 }
