074e89b523e87d41189050bda22b0cb1ae8d7247
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-15 15:26:40 +0300

    DeveloperController.cs  v2, TaskController.cs v1

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -4,7 +4,7 @@ using Task = Case2GK20221102.Entities.Task;
 
 namespace Case2GK20221102.Controllers;
 
-public class DeveloperController
+public class DeveloperController    
 {
     private DeveloperService _developerService;
     private TaskService _taskService;
@@ -17,20 +17,15 @@ public class DeveloperController
 
     public void DeleteTaskDeveloperId(string[] developerParts)
     {
-        if (developerParts[1] == "dev" && developerParts[0] == "delete")
-        {
+        // if (developerParts[1] == "dev" && developerParts[0] == "delete")
+        // {
             var task = new Task();
             if (developerParts[2] == Convert.ToString(task.DeveloperId))
             {
                 task.DeveloperId = Guid.Empty;
             }
             _developerService.Delete(developerParts);
-        }
-        
-        
-
-       
-
+        // }
         
     }
 }
--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,7 +1,13 @@
+using System.Data;
+using System.Reflection.Metadata;
+using Case2GK20221102.Entities;
 using Case2GK20221102.Services;
+using Task = Case2GK20221102.Entities.Task;
 
 namespace Case2GK20221102.Controllers;
 
+//assign işleminde ilgili developerın var olduğunu doğrulamak.
+//Task ile developer’ın departmanlarının aynı olduğunu doğrulamak
 public class TaskController
 {
     private DeveloperService _developerService;
@@ -12,4 +18,29 @@ public class TaskController
         _developerService = developerService;
         _taskService = taskService;
     }
+    public void TaskAdd(string[] taskParts)   //Guid
+    {  
+    }
+    
+    public bool TaskAssign(string[] taskParts)
+    {   
+        var developer = new Developer();
+        var task = new Task();
+       
+        if (taskParts[7] == Convert.ToString(developer.Id) && 
+            taskParts[5] ==Convert.ToString(developer.Department) &&
+            taskParts[2] == Convert.ToString(task.Id))
+        {
+           _taskService.Update(taskParts); 
+           //Update,task,Id,title,description,department,status,DeveloperId
+        }
+        if (taskParts[7] == Convert.ToString(developer.Id) && 
+            taskParts[5] ==Convert.ToString(developer.Department))
+        {
+            _taskService.Add(taskParts); 
+        }
+        return true;
+    }
+
+
 } 
