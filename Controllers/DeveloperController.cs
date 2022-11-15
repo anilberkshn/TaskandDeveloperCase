bf96594527fca13a306d1ed6dba3f11df2800755
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-15 15:44:55 +0300

    kodun tekrar incelemesi sırasında görüntü düzeni

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -3,7 +3,7 @@ using Task = Case2GK20221102.Entities.Task;
 
 namespace Case2GK20221102.Controllers;
 
-public class DeveloperController    
+public class DeveloperController
 {
     private DeveloperService _developerService;
     private TaskService _taskService;
@@ -18,13 +18,13 @@ public class DeveloperController
     {
         // if (developerParts[1] == "dev" && developerParts[0] == "delete")
         // {
-            var task = new Task();
-            if (developerParts[2] == Convert.ToString(task.DeveloperId))
-            {
-                task.DeveloperId = Guid.Empty;
-            }
-            _developerService.Delete(developerParts);
+        var task = new Task();
+        if (developerParts[2] == Convert.ToString(task.DeveloperId))
+        {
+            task.DeveloperId = Guid.Empty;
+        }
+
+        _developerService.Delete(developerParts);
         // }
-        
     }
 }
--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -16,29 +16,30 @@ public class TaskController
         _developerService = developerService;
         _taskService = taskService;
     }
-    public void TaskAdd(string[] taskParts)   //Guid
-    {  
+
+    public void TaskAdd(string[] taskParts) //Guid
+    {
     }
-    
+
     public bool TaskAssign(string[] taskParts)
-    {   
+    {
         var developer = new Developer();
         var task = new Task();
-       
-        if (taskParts[7] == Convert.ToString(developer.Id) && 
-            taskParts[5] ==Convert.ToString(developer.Department) &&
+
+        if (taskParts[7] == Convert.ToString(developer.Id) &&
+            taskParts[5] == Convert.ToString(developer.Department) &&
             taskParts[2] == Convert.ToString(task.Id))
         {
-           _taskService.Update(taskParts); 
-           //Update,task,Id,title,description,department,status,DeveloperId
+            _taskService.Update(taskParts);
+            //Update,task,Id,title,description,department,status,DeveloperId
         }
-        if (taskParts[7] == Convert.ToString(developer.Id) && 
-            taskParts[5] ==Convert.ToString(developer.Department))
+
+        if (taskParts[7] == Convert.ToString(developer.Id) &&
+            taskParts[5] == Convert.ToString(developer.Department))
         {
-            _taskService.Add(taskParts); 
+            _taskService.Add(taskParts);
         }
+
         return true;
     }
-
-
-} 
+}
