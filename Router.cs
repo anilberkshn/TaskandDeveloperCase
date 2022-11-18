77b8978eaf14e7f3554da790b43fe2c6278fed03
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-18 13:39:41 +0300

    router constracter

--- Router.cs
+++ Router.cs
@@ -1,6 +1,15 @@
+using Case2GK20221102.Controllers;
+
 namespace Case2GK20221102;
 
 public class Router
 {
-    
+    private DeveloperController _developerController;
+    private TaskController _taskController;
+
+    public Router(DeveloperController developerController, TaskController taskController)
+    {
+        _developerController = developerController;
+        _taskController = taskController;
+    }
 }
