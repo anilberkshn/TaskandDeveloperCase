f650eea71da938cd9d6e1d7087bf9b7c887a021b
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-22 18:27:11 +0300

    DeveloperController.cs, AddDeveloper,DeleteDeveloper method

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -1,3 +1,4 @@
+using Case2GK20221102.Entities;
 using Case2GK20221102.Services;
 using Task = Case2GK20221102.Entities.Task;
 
@@ -13,12 +14,64 @@ public class DeveloperController
         _developerService = developerService;
         _taskService = taskService;
     }
+    //
+    // Developer service add methodu
+    // Add,developer,name,surname,departmant
+    //     DeveloperService Update Method
+    // Update,developer,Id,name,surname,department
+    //     DeveloperService Delete Method
+    // Delete,Developer,Id
+    //     
+    public void AddDeveloper(string[] developerParts) // Add,developer,name,surname,departmant
+    {
+        //0-1 ve bir partı routerda yönlenmesi gerekli diye düşündüm. 
+        // todo: servis kısmında add de oluşturulacak nesne için buradan doğru gitmesi
+        if (developerParts[2] == null && developerParts[3] == null )
+        {
+            throw new Exception("ad soyad boş olamaz.");
+        }
+        else if (developerParts[4] is not ("0" or "1" or "2"))
+        {
+            throw new Exception("Developer Departmanı 0,1,2 olmalıdır.");
+        }
+        else
+        {
+            _developerService.Add(developerParts);
+        }
+    }
+
+    public void UpdateDeveloper(string[] developerParts)
+    {
+        
+    }
+
+    public void DeleteDeveloper(string[] developerParts) // Delete,Developer,Id
+    {
+        var developers = _developerService.GetAll();
+        foreach (var dev in developers)
+        {
+            if (dev.Id == Guid.Parse(developerParts[2]))
+            {
+                _developerService.Delete(developerParts);
+            }
+        }
+
+        throw new Exception("Girilen id değerine ait kullanıcı bulunamamıştır");
+    }
+
+
+    public Developer GetDeveloper(string id)
+    {
+        // todo: Ne yapacağını tekrar düşüneceğim, return hata vermesin diye yazdım. 
+        return _developerService.Get(id);
+    }
+
 
     public void DeleteTaskDeveloperId(string[] developerParts)
     {
         // if (developerParts[1] == "dev" && developerParts[0] == "delete")
         // {
-        var task = new Task();
+        var task = new Task(); // burada bomboş yeni bir task oluşturmak yanlış. 
         if (developerParts[2] == Convert.ToString(task.DeveloperId))
         {
             task.DeveloperId = Guid.Empty;
@@ -29,7 +82,6 @@ public class DeveloperController
 
         foreach (var t in tasks)
         {
-            
         }
         // }
     }
@@ -38,9 +90,7 @@ public class DeveloperController
     //parçaları valide et if if diye validasyondan geçenmeyenler için error fırlat
     // Errorların custom error oluşturma 
     // bu şekilde crud işlemleri tamamlanacak.
-    
+
     //2. aşama
     // 1 dev silindiğinde ()
-    
-    
 }
