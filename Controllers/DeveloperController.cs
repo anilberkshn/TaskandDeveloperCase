fcaa5f9ac97562f663f1821f4af8c0fed873fbdd
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-11-25 16:18:04 +0300

    DeveloperController.cs kod düzenlenme, 2.kısım öncesi

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -1,7 +1,6 @@
 using Case2GK20221102.Entities;
 using Case2GK20221102.Errors;
 using Case2GK20221102.Services;
-using Task = Case2GK20221102.Entities.Task;
 
 namespace Case2GK20221102.Controllers;
 
@@ -15,11 +14,11 @@ public class DeveloperController
         _developerService = developerService;
         _taskService = taskService;
     }
-   
+
     public Guid AddDeveloper(string[] developerParts) // Add,developer,name,surname,departmant
     {
         // todo: 0-1 ve bir partı routerda yönlenmesi gerekli diye düşündüm. 
-        
+
         if (developerParts[2].Length < 2)
         {
             throw new ValidationErrorException();
@@ -32,25 +31,25 @@ public class DeveloperController
         {
             throw new Exception("Developer Departmanı 0,1,2 olmalıdır.");
         }
-        
+
         return _developerService.Add(developerParts);
     }
-    
+
     public Developer GetDeveloper(string[] developerParts)
         //Service string id istiyor gelen partı bölüp gönderdim.
         //Get,Develepor,Id program girişinde beklenen.
     {
         return _developerService.Get(developerParts[2]) ?? throw new DeveloperNotFoundException();
     }
-    
-    public bool UpdateDeveloper(string[] developerParts) 
+
+    public bool UpdateDeveloper(string[] developerParts)
         // Update 0,developer 1 ,Id 2 ,name 3 ,surname 4 ,department 5
-       {
-        if (developerParts[2].Length < 2 )
+    {
+        if (developerParts[2].Length < 2)
         {
-            throw new ValidationErrorException(); 
+            throw new ValidationErrorException();
         }
-        else if (developerParts[3].Length < 2 )
+        else if (developerParts[3].Length < 2)
         {
             throw new ValidationErrorException();
         }
@@ -62,51 +61,16 @@ public class DeveloperController
         {
             throw new ValidationErrorException();
         }
-       
+
         return _developerService.Update(developerParts);
     }
 
     public bool DeleteDeveloper(string[] developerParts) // Delete,Developer,Id
     {
-        // try
-        // {
-            // int sayac = 0; 
-            // var developers = _developerService.GetAll();
-            // foreach (var dev in developers)
-            // {
-            //     if (dev.Id == Guid.Parse(developerParts[2]))
-            //     {
-            //         _developerService.Delete(developerParts);
-            //         sayac++;
-            //     }
-            // }
-            //
-            // if (sayac == 0)
-            // {
-            //     throw new Exception("Girilen id değerine ait kullanıcı bulunamamıştır");
-            // }
-
-            return _developerService.Delete(developerParts); 
-            //// Her zaman true dönecek. 
-
-            // }
-            // catch (Exception )
-            // {
-            //     Console.WriteLine("Girilen id değerine ait kullanıcı bulunamamıştır");
-            //     throw;
-            // }
-
-            // sürekli hata verir.
-            //throw new Exception("Girilen id değerine ait kullanıcı bulunamamıştır");
+        return _developerService.Delete(developerParts);
     }
 
 
-    
-    
-    
-    
-    
-    
     //*********************************2. KISIM 
 
     // public void DeleteTaskDeveloperId(string[] developerParts)
@@ -136,3 +100,39 @@ public class DeveloperController
     //2. aşama
     // 1 dev silindiğinde ()
 }
+
+
+//********************** Delete metot denemeler
+// public bool DeleteDeveloper(string[] developerParts) // Delete,Developer,Id
+// {
+//     // try
+//     // {
+//     // int sayac = 0; 
+//     // var developers = _developerService.GetAll();
+//     // foreach (var dev in developers)
+//     // {
+//     //     if (dev.Id == Guid.Parse(developerParts[2]))
+//     //     {
+//     //         _developerService.Delete(developerParts);
+//     //         sayac++;
+//     //     }
+//     // }
+//     //
+//     // if (sayac == 0)
+//     // {
+//     //     throw new Exception("Girilen id değerine ait kullanıcı bulunamamıştır");
+//     // }
+//
+//     return _developerService.Delete(developerParts); 
+//     //// Her zaman true dönecek. 
+//
+//     // }
+//     // catch (Exception )
+//     // {
+//     //     Console.WriteLine("Girilen id değerine ait kullanıcı bulunamamıştır");
+//     //     throw;
+//     // }
+//
+//     // sürekli hata verir.
+//     //throw new Exception("Girilen id değerine ait kullanıcı bulunamamıştır");
+// }
